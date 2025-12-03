401bedf4f5155c67ffb82cb21ae0a7873f5fd531
KasunBandara921 <bandarakasun495@gmail.com>
2025-12-03 21:46:19 +0530

    React vite Practise with Mcodefrontend Youtube Channel

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css';
+
+  
+
+function  App() {
+
+  const stylesdev = {
+    color: 'blue',
+    fontSize: '20px',
+    backgroundColor: 'lightgray',
+    marginTop: '20px'
+  };
+
+  return (
+    <>
+      <div id="wrapper">
+
+      
+
+        
+      
+        
+        
+
+        
+
+       
+
+       
+      </div>
+
+    </>
+  )
+}
+
+export default App
